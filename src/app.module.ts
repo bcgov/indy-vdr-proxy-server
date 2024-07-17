3489d256685ad0dac4404cd353255a0f04fa89e5
Bryce McMath <bryce.j.mcmath@gmail.com>
2024-07-16 20:38:57 -0700

    fix: add our own agent creation, increase log level
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,17 @@ import { AppService } from './app.service'
 import { ConsoleLogger } from '@credo-ts/core'
 import { IndyVdrPoolConfig } from '@credo-ts/indy-vdr'
 import { Logger } from '@nestjs/common'
-import { IndyVdrProxyModule, setupAgent } from 'credo-ts-indy-vdr-proxy-server'
+import { IndyVdrProxyModule } from 'credo-ts-indy-vdr-proxy-server'
 import { readFileSync } from 'fs'
 import 'dotenv/config'
+
+import { setupAgent } from './helpers/agent'
 
 const configPath = process.env.INDY_VDR_PROXY_CONFIG_PATH ?? './res/app.config.json'
 Logger.log(`Registering Indy VDR Proxy Module with config file ${configPath}`)
 const config = JSON.parse(readFileSync(configPath, { encoding: 'utf-8' }))
 const networks = config.networks as [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]
-const logLevel = process.env.LOG_LEVEL ? parseInt(process.env.LOG_LEVEL) : 3
+const logLevel = process.env.LOG_LEVEL ? parseInt(process.env.LOG_LEVEL) : 2
 const ttl = process.env.THROTTLE_TTL ? parseInt(process.env.THROTTLE_TTL) : 60000
 const limit = process.env.THROTTLE_LIMIT ? parseInt(process.env.THROTTLE_LIMIT) : 2000
 Logger.log(`Using Credo log level ${logLevel}, throttle TTL ${ttl}, and throttle limit ${limit}`)
---
+++ src/helpers/agent.ts
@@ -0,0 +1,52 @@
+import { Agent, ConsoleLogger, DidsModule, Logger, LogLevel } from '@credo-ts/core'
+import { agentDependencies } from '@credo-ts/node'
+import { AskarModule } from '@credo-ts/askar'
+import { AnonCredsModule } from '@credo-ts/anoncreds'
+import {
+  IndyVdrAnonCredsRegistry,
+  IndyVdrModule,
+  IndyVdrPoolConfig,
+  IndyVdrSovDidResolver,
+} from '@credo-ts/indy-vdr'
+import { anoncreds } from '@hyperledger/anoncreds-nodejs'
+import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
+import { indyVdr } from '@hyperledger/indy-vdr-nodejs'
+
+export type IndyVdrProxyAgent = Agent<ReturnType<typeof getIndyVdrProxyAgentModules>>
+
+export function setupAgent(options: {
+  networks: [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]
+  logger?: Logger
+}) {
+  return new Agent({
+    config: {
+      label: 'Indy VDR Proxy',
+      walletConfig: {
+        id: 'indy-vdr-proxy',
+        key: 'indy-vdr-proxy',
+      },
+      autoUpdateStorageOnStartup: true,
+      backupBeforeStorageUpdate: false,
+      logger: options.logger ?? new ConsoleLogger(LogLevel.debug),
+    },
+    dependencies: agentDependencies,
+    modules: getIndyVdrProxyAgentModules(options.networks),
+  })
+}
+
+const getIndyVdrProxyAgentModules = (networks: [IndyVdrPoolConfig, ...IndyVdrPoolConfig[]]) => {
+  return {
+    askar: new AskarModule({
+      ariesAskar,
+    }),
+    anoncreds: new AnonCredsModule({
+      registries: [new IndyVdrAnonCredsRegistry()],
+      anoncreds,
+    }),
+    dids: new DidsModule({ resolvers: [new IndyVdrSovDidResolver()] }),
+    indyVdr: new IndyVdrModule({
+      indyVdr,
+      networks,
+    }),
+  } as const
+}
